f39e1d66502e1f3fc0478cac3bb1806a54a8ef3d
IlyaKirichenko <ikirichenko345@gmail.com>
2026-02-21 10:53:28 +0500

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    cout << "=== КЛИЕНТ === " << endl;
+    cout << "Выберите приоритет клиента: " << endl;
+    cout << "1. Фоновый " << endl;
+    cout << "2. Нормальный " << endl;
+    cout << "3. Высокий " << endl;
+
+    int choise;
+    cin >> choise;
+
+    if (choise > 3 || choise < 1) {
+        cout << "Введите число от 1 до 3 " << endl;
+        choise = 2;
+    }
+
+    HANDLE PrProc = GetCurrentProcess();
+
+    switch (choise)
+    {
+    case 1:
+        SetPriorityClass(PrProc, IDLE_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Фоновый " << endl;
+        break;
+    case 2:
+        SetPriorityClass(PrProc, NORMAL_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Нормальный " << endl;
+        break;
+    case 3:
+        SetPriorityClass(PrProc, HIGH_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Высокий " << endl;
+        break;
+    }
+
+    HANDLE PrinterMutex = OpenMutex(SYNCHRONIZE, FALSE, L"PrinterMutex");
+
+    if (PrinterMutex == NULL) {
+        cout << "Принтер не найден" << endl;
+        return GetLastError();
+    }
+    else {
+        cout << "Принтер обнаружен " << endl;
+    }
+
+    while (true)
+    {
+        cout << "\nНажмите Enter для печати" << endl;
+        cin.get();
+
+        WaitForSingleObject(PrinterMutex, INFINITE);
+        cout << "Задание отправлено " << endl;
+        Sleep(100);            
+        ReleaseMutex(PrinterMutex);
+    }
+
+
+    return 0;
+}
---
+++ Printer.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    cout << "=== ПРИНТЕР === " << endl;
+
+    HANDLE PrinterMutex = CreateMutex(NULL, FALSE, L"PrinterMutex");
+
+    if (PrinterMutex == NULL) {
+        cout << "Ошибка создания мьютекса! " << endl;
+        return GetLastError();
+    }
+
+    cout << "Принтер ждет клиентов " << endl;
+
+    wchar_t appName[] = L"C:\\Users\\st310-02\\Documents\\Kirichenko_PR-31\\Pract_12_SP\\Pract_12_SP\\Printer\\x64\\Debug\\Client.exe";
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    if (!CreateProcess(NULL, appName, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        cout << "Процесс не был запущен" << endl;
+        return GetLastError();
+    }
+    else {
+        cout << "ПРИНТЕР: Клиент запущен " << endl;
+    }
+
+    while (true) {
+        int closePrinter = WaitForSingleObject(PrinterMutex, 60000);
+
+        if (closePrinter == WAIT_TIMEOUT) {
+            cout << "ПРИНТЕР: Ошибка печати " << endl;
+            break; 
+        }
+
+        if (closePrinter == WAIT_OBJECT_0) {
+            cout << "ПРИНТЕР: Печатаю... " << endl;
+
+            int printTime = 5000 + rand() % 5000;
+            Sleep(printTime);
+
+            cout << "ПРИНТЕР: Закончил печать " << endl;
+
+            ReleaseMutex(PrinterMutex);
+        }
+    }
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    CloseHandle(PrinterMutex);
+    cout << "ПРИНТЕР: Выключен " << endl;
+    return 0;
+}
